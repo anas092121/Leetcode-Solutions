522d28632fbf78ddbf490104af08272c346b1961
Mohammad Anas <anas092121@gmail.com>
2024-11-20 22:15:05 +0530

    Time: 0 ms (100%), Space: 10.5 MB (61.95%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
+        int count = 0;
         vector<int> temp = heights;
         sort(heights.begin(), heights.end());
-        int count = 0;
         for(int i=0; i<heights.size(); i++){
             if(heights[i]!=temp[i])
                 count++;

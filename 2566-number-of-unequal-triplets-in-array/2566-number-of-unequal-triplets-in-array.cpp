a03bde32fcba4eb9ac5495e42936729e5fb1bda1
Mohammad Anas <anas092121@gmail.com>
2024-11-21 09:34:55 +0530

    Time: 0 ms (100%), Space: 11.6 MB (6.18%) - LeetHub

--- 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.cpp
+++ 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-        int unequalTriplets(vector<int>& A) {
+    int unequalTriplets(vector<int>& nums) {
         int trips = 0, pairs = 0, count[1001] = {};
-        for (int i = 0; i < A.size(); ++i) {
-            trips += pairs - count[A[i]] * (i - count[A[i]]);
-            pairs += i - count[A[i]];
-            count[A[i]] += 1;
+        for (int i = 0; i < nums.size(); ++i) {
+            trips += pairs - count[nums[i]] * (i - count[nums[i]]);
+            pairs += i - count[nums[i]];
+            count[nums[i]] += 1;
         }
         return trips;
     }

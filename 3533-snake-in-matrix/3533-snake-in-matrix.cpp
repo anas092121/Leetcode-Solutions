6ea056c5b613e20e00f3cd9e2ee10997bb8f512e
Mohammad Anas <anas092121@gmail.com>
2024-11-25 14:43:42 +0530

    Time: 0 ms (100%), Space: 35 MB (91.35%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int finalPositionOfSnake(int n, vector<string>& commands) {
+        int row = 0;
+        int col = 0;
+        for(int i=0; i<commands.size(); i++){
+            if(commands[i] == "UP"){
+                row--;
+            }
+            else if(commands[i] == "DOWN"){
+                row++;
+            }
+            else if(commands[i] == "RIGHT"){
+                col++;
+            }
+            else{
+                col--;
+            }
+        }
+        return (row * n) + col;
+    }
+};

36fdf4f5ed277c2a8f7ea468cb44280b076956c0
Mohammad Anas <anas092121@gmail.com>
2024-12-04 12:03:48 +0530

    Time: 3 ms (7.84%), Space: 9.4 MB (6.65%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.cpp
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     // Use sets for faster lookup
-    unordered_set<char> first = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'};
-    unordered_set<char> second = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'};
-    unordered_set<char> third = {'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'};
+    unordered_set<char> first = {'q', 'w', 'e', 'r', 't', 'y', 'u','i', 'o', 'p', 'Q', 'W', 'E', 'R','T', 'Y', 'U', 'I', 'O', 'P'};
+    unordered_set<char> second = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l','A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'};
+    unordered_set<char> third = {'z', 'x', 'c', 'v', 'b', 'n', 'm','Z', 'X', 'C', 'V', 'B', 'N', 'M'};
 
-    bool canBeTypedFromRow(const string& word, const unordered_set<char>& rowSet) {
+    bool canBeTypedFromRow(const string& word,
+                           const unordered_set<char>& rowSet) {
         for (char ch : word) {
             if (rowSet.find(ch) == rowSet.end()) {
                 return false;
@@ -17,7 +18,9 @@ public:
     vector<string> findWords(vector<string>& words) {
         vector<string> ans;
         for (const string& word : words) {
-            if (canBeTypedFromRow(word, first) || canBeTypedFromRow(word, second) || canBeTypedFromRow(word, third)) {
+            if (canBeTypedFromRow(word, first) ||
+                canBeTypedFromRow(word, second) ||
+                canBeTypedFromRow(word, third)) {
                 ans.push_back(word);
             }
         }

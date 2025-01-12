77f8ac4604f2f6f9909cac576ddc1d3bb6d7721b
Mohammad Anas <anas092121@gmail.com>
2025-01-12 20:30:50 +0530

    Time: 0 ms (100%), Space: 7.8 MB (60.43%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        while(n){
+            if(n%2 == 0){
+                n = n/2;
+            }
+            else if(n%3 == 0){
+                n = n/3;
+            }
+            else if(n%5 == 0){
+                n = n/5;
+            }else{
+                return n==1;
+            }
+        }
+        return false;
+    }
+};

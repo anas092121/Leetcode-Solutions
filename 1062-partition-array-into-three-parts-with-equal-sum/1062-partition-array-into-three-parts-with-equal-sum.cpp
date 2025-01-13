e1b1c71cadb9552097dc5571b5c59db24cdbe38e
Mohammad Anas <anas092121@gmail.com>
2025-01-13 18:39:03 +0530

    Time: 0 ms (100%), Space: 36.3 MB (41.23%) - LeetHub

--- 1062-partition-array-into-three-parts-with-equal-sum/1062-partition-array-into-three-parts-with-equal-sum.cpp
+++ 1062-partition-array-into-three-parts-with-equal-sum/1062-partition-array-into-three-parts-with-equal-sum.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     bool canThreePartsEqualSum(vector<int>& arr) {
         int sum = 0;
-
-        // Calculate the total sum of the array
         for (int num : arr) {
             sum += num;
         }
-
-        // If the sum is not divisible by 3, return false
         if (sum % 3 != 0) {
             return false;
         }
-
         int target = sum / 3;
         int parts = 0, currentSum = 0;
 
-        // Iterate through the array to find partitions
         for (int num : arr) {
             currentSum += num;
 
@@ -25,13 +19,10 @@ public:
                 currentSum = 0;
             }
 
-            // If we find 3 parts, return true
             if (parts == 3) {
                 return true;
             }
         }
-
-        // If less than 3 parts are found, return false
         return false;
     }
 };

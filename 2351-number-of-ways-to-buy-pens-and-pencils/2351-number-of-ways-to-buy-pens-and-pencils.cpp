75e99a02bc74379fb36a45d91800c32bab01fcd7
Mohammad Anas <anas092121@gmail.com>
2025-02-15 23:39:35 +0530

    Time: 8 ms (65.12%), Space: 8 MB (59.59%) - LeetHub

--- 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -4,8 +4,7 @@ public:
         long long ans = 0;
         int pen = total / cost1;
         for (int i = 0; i <= pen; i++) {
-            int t = total;
-            t = t - (cost1 * i);
+            int t = total - (cost1 * i);
             int pencil = t / cost2;
             ans += pencil + 1;
         }

a73b1e601d6b67dca6c27fcea0b8d43f45804023
Mohammad Anas <anas092121@gmail.com>
2025-03-27 09:33:53 +0530

    Time: 11 ms (34.18%), Space: 140 MB (77.06%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,23 +1,49 @@
+// class Solution {
+// public:
+//     int minOperations(vector<int>& nums) {
+//         deque<int> flipQ;
+//         int count = 0;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             while (!flipQ.empty() && i > flipQ.front() + 2) {
+//                 flipQ.pop_front();
+//             }
+
+//             if ((nums[i] + flipQ.size()) % 2 == 0) {
+//                 if (i + 2 >= nums.size()) {
+//                     return -1;
+//                 }
+//                 count++;
+//                 flipQ.push_back(i);
+//             }
+//         }
+
+//         return count;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        deque<int> flipQueue;
         int count = 0;
+        for(int i=2; i<nums.size(); i++){
+            if(nums[i-2] == 0){
+                nums[i-2] = 1;
+                nums[i-1] ^= 1;
+                nums[i] ^= 1;
 
-        for (int i = 0; i < nums.size(); i++) {
-            while (!flipQueue.empty() && i > flipQueue.front() + 2) {
-                flipQueue.pop_front();
-            }
-
-            if ((nums[i] + flipQueue.size()) % 2 == 0) {
-                if (i + 2 >= nums.size()) {
-                    return -1;
-                }
                 count++;
-                flipQueue.push_back(i);
             }
         }
 
-        return count;
+        int sum = 0;
+        for(auto it : nums){
+            sum += it;
+        }
+
+        return sum==nums.size() ? count : -1;
     }
 };

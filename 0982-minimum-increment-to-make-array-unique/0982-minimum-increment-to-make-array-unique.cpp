fb6cf2fef65d37ebe10ee50975e447adafe220cc
Mohammad Anas <anas092121@gmail.com>
2025-04-09 11:43:08 +0530

    Time: 854 ms (5.09%), Space: 69.6 MB (27.03%) - LeetHub

---
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for(auto int i=1; i<nums.size(); i++){
+            if(nums[i] <= nums[i-1]){
+                int temp = abs(nums[i] - nums[i-1]) + 1;
+                ans += temp;
+                cout<< temp<< " ";
+                nums[i] = nums[i-1] + 1;
+            }
+        }
+        return ans;
+    }
+};

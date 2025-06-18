f978f7570edd37f60e8cd3cd1aa54a813ee17a34
Mohammad Anas <anas092121@gmail.com>
2025-06-18 12:15:39 +0530

    Time: 0 ms (100%), Space: 13.4 MB (46.06%) - LeetHub

--- 2020-remove-one-element-to-make-the-array-strictly-increasing/2020-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 2020-remove-one-element-to-make-the-array-strictly-increasing/2020-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool canBeIncreasing(vector<int>& nums) {
-        int count = 0;  // Count of elements to remove
+        int count = 0; 
         for (int i = 1; i < nums.size(); ++i) {
             if (nums[i] <= nums[i - 1]) {
                 count++;
                 if (count > 1)
                     return false;
 
-                // Decide whether to remove nums[i-1] or nums[i]
                 if (i > 1 && nums[i] <= nums[i - 2]) {
-                    nums[i] = nums[i - 1]; // Pretend nums[i] is removed
+                    nums[i] = nums[i - 1];
                 }
-                // else pretend nums[i - 1] is removed (do nothing)
             }
         }
         return true;

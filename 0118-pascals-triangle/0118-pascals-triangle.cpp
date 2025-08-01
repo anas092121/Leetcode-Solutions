1279bd1e783920ba73d32e12f8071326c0bdeceb
Mohammad Anas <anas092121@gmail.com>
2025-08-01 22:41:08 +0530

    Time: 0 ms (100%), Space: 9.7 MB (30.63%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int>> ans;
         for(int i=0; i<numRows;i++){
             vector<int> row(i+1,1);
-            for(int j=1;j<i;j++){   //it will start when i=2
+            for(int j=1;j<i;j++){  
                 row[j]= ans[i-1][j-1]+ans[i-1][j];
             }
             ans.push_back(row);

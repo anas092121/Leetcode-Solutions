a37b0cceebd2d05dcf7b6984cd55fae2aa470199
Mohammad Anas <anas092121@gmail.com>
2025-09-09 14:56:55 +0530

    Time: 0 ms (100%), Space: 12.3 MB (63.25%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int n = citations.size();
+        int h = 0;
+        for (int i = 0; i < n; i++) {
+            int total = n - i;
+            h = max(h, min(citations[i], total));
+        }
+        return h;
+    }
+};

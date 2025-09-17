86d90bdbb0895bbd3b550259c1680fa3617d6f23
Mohammad Anas <anas092121@gmail.com>
2025-09-17 09:44:22 +0530

    Time: 19 ms (72.62%), Space: 94.5 MB (55.57%) - LeetHub

---
+++ 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean containsCycle(char[][] grid) {
+        boolean[][] seen = new boolean[grid.length][grid[0].length];
+            
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (!seen[i][j] && search(grid, grid[i][j], i, j, seen)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean search(char[][] grid, char target, int i, int j, boolean[][] seen) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != target) {
+            return false;
+        }
+        
+        if (seen[i][j]) {
+            return true;
+        }
+        
+        seen[i][j] = true;
+        grid[i][j] = '#';
+        
+        boolean found =  search(grid, target, i + 1, j, seen)
+                    ||  search(grid, target, i - 1, j, seen)
+                    ||  search(grid, target, i, j - 1, seen)
+                    ||  search(grid, target, i, j + 1, seen);
+
+        grid[i][j] = target;
+        
+        return found;
+    }
+}

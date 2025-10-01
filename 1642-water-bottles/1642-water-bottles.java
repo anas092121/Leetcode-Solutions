a1d0d09c9d670dbef65497b946fb485b748881f3
Mohammad Anas <anas092121@gmail.com>
2025-10-01 10:25:48 +0530

    Time: 0 ms (100%), Space: 40.6 MB (43.3%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int consumedBottles = 0;
+
+        while (numBottles >= numExchange) {
+            int K = numBottles / numExchange;
+
+            consumedBottles += numExchange * K;
+            numBottles -= numExchange * K;
+            numBottles += K;
+        }
+        return consumedBottles + numBottles;
+    }
+}
